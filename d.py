b880e995a31e59c2c5bd20ee95b11e64a0ddd120
moyshik7 <moyshik7@gmail.com>
2021-09-09 00:50:32 +0600

    Going further

--- d.py
+++ d.py
@@ -1,10 +1,16 @@
 import os
 import discord
 from dotenv import load_dotenv
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot("Imouto",
+    database_uri="sqlite:///database.db"
+)
+client = discord.Client()
+trainer = ListTrainer(bot) 
 
 load_dotenv()
-
-client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -16,8 +22,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("train"):
+        await message.channel.send("train me oni chan")
+    if message.content.startswith("test"):
+        await message.channel.send("Owo testing")
 
 
 client.run(os.getenv("BOT_TOKEN"))
